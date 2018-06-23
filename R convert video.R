ce41e2f4fad10962c375266a88ec538a5fe9a0d8
Brandon <34313332+bcwralph@users.noreply.github.com>
2018-06-22 21:46:07 -0400

    Update R convert video.R

--- R convert video.R
+++ R convert video.R
@@ -1,5 +1,5 @@
 #convert videos using ffmpeg into a '/trimmed' folder
-#codex properties used here are for use with pygame.movie
+#setup to convert pygame.movie friendly files
 library(gtools)
 
 #mass convert
@@ -7,13 +7,13 @@ myPath = 'C:/Users/You/'
 my_dir_list = mixedsort(list.files(myPath))
 
 #list of start and end times of format 00:00:00
-start_times = c("")
-end_times = c("")
+start_times = c("00:00:00")
+end_times = c("00:00:30")
 for (i in 1:length(start_times)) {
   system(sprintf('ffmpeg -i "%s%s" -vcodec mpeg1video -q:v 6 -acodec libmp3lame -r 30 -vf scale=500:400 -avoid_negative_ts make_zero -ss %s -to %s -intra trimmed/video_%s.mpg',myPath, my_dir_list[i],start_times[i],end_times[i],as.character(i)))
 }
 
-#single convert, where i is item index in directory
-i=24
+#convert single within dir, where i is item index in directory
+i=0
 system(sprintf('ffmpeg -i "%s%s" -vcodec mpeg1video -q:v 6 -acodec libmp3lame -r 30 -vf scale=500:400 -avoid_negative_ts make_zero -ss %s -to %s -intra trimmed/video_%s.mpg',myPath, my_dir_list[i],start_times[i],end_times[i],as.character(i)))
 
